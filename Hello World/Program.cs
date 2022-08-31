95bc6eeba6f3576915776ad8e636853bf35b4c09
namigabilov <namigna@code.edu.az>
2022-08-31 19:23:54 +0400

    Hello World

---
+++ Hello World/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Hello World :)");
+                
+            }
+            Console.Read();
+        }
+    }
+}
