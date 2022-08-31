aa7f632927f272362b17fbef34d322576afe24dd
namigabilov <namigna@code.edu.az>
2022-08-31 19:06:06 +0400

    1-100 ededlerin arasinda cut eledlerin cemi

---
+++ 1-100cut ededlerin cemi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1_100cut_ededlerin_cemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short sum=0;
+            for (short i = 0; i <=100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum) ;
+        }
+    }
+}
