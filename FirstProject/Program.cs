c3479b3e4240bd123f7f8dd24be351288f238217
namigabilov <namigna@code.edu.az>
2022-08-31 18:58:51 +0400

    Proje dosyası ekle.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SadeMurekkeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 121, n = 1, count = 0;
+            while (n < a)
+            {
+                if (a % n == 0)
+                {
+                    count++;
+                }
+                n++;
+            }
+            if (count >1)
+            {
+                Console.WriteLine(a + "  Murekkebdir");
+            }
+            else
+            {
+                Console.WriteLine(a + "  Sadedir:");
+            }
+
+        }
+    }
+}
