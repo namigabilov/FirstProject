6ec1c3f7fc7ac62601db0268b0e357c7a1f2a348
namigabilov <namigna@code.edu.az>
2022-08-31 19:13:49 +0400

    Ededin reqemleri

---
+++ Ededin reqemli/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ededin_reqemli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 217, num=0;
+
+            while (a > 1)
+            {
+                num = a % 10;
+                Console.WriteLine(num);
+                a = (a - num) / 10;
+
+            }
+        }
+    }
+}
